dd73f2e566df9ce84d3a9e255bdf4faa6bcf00d1
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2024-09-10 13:22:58 +0530

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Main{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter your First number:");
+	    int a = sc.nextInt();
+	    System.out.println("Enter your Second number:");
+	    int b = sc.nextInt();
+	    if (b!=0){
+	        float c = a / b;
+	        System.out.println("The Solution is: "+c);
+	    }
+	    else {
+	        System.out.println("Invalid Usage");
+	    }
+	}
+}
