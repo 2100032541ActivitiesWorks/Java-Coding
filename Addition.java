c3fb64fa899dcd807ae968a5fac438e24734000d
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2024-09-10 13:18:24 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter your First number:");
 	    int a = sc.nextInt();
-	    System.out.println("Enter your First number: "+a);
+	    System.out.println("Enter your Second number:");
 	    int b = sc.nextInt();
-	    System.out.println("Enter your Second number: "+b);
 	    int c = a + b;
 	    System.out.println("The Total solution is: "+c);
 	    sc.close();
