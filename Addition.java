6a4dc1ca0936aa188ef1b8103a646e6249081782
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2024-09-10 13:14:51 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    System.out.println("Enter your First number: "+a);
+	    int b = sc.nextInt();
+	    System.out.println("Enter your Second number: "+b);
+	    int c = a + b;
+	    System.out.println("The Total solution is: "+c);
+	    sc.close();
+	}
+}
